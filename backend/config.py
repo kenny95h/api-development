b7e7f7d05a53cab192e1a18d24ffde731a0a146f
kenny95h <108397966+kenny95h@users.noreply.github.com>
2024-11-14 13:41:48 +0000

    added back config file

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+# THIS FILE WAS ADDED TO .gitignore SO IT WOULD NOT BE UPLOADED 
+# AS IT CONTAINS LOGIN DETAILS. ADDED BACK AS REQUESTED
+
+test_db_user = 'postgres'
+test_db_pword = 'password'
+db_user = 'postgres'
+db_pword = 'password'
